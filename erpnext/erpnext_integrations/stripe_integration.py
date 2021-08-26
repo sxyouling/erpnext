9606520064c351386bca75a6e0de39638324d8d9
Rohan <Alchez@users.noreply.github.com>
2021-08-26 21:47:00 +0530

    fix: use Stripe's Price API for plan-price information (#26107)
    * fix: use Stripe's new Plan API for price information
    
    * patch: use inbuilt function to rename field
    
    * fix: patch call
    
    Co-authored-by: Ankush Menat <ankush@iwebnotes.com>
    Co-authored-by: Nabin Hait <nabinhait@gmail.com>
    (cherry picked from commit 16eed07a0fb34b8d0ad042ed18d79a1123cc6076)
    
    # Conflicts:
    #	erpnext/accounts/doctype/subscription_plan/subscription_plan.json
    #	erpnext/erpnext_integrations/stripe_integration.py
    #	erpnext/patches.txt

--- erpnext/erpnext_integrations/stripe_integration.py
+++ erpnext/erpnext_integrations/stripe_integration.py
@@ -1,6 +1,11 @@
 # Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
+<<<<<<< HEAD
+=======
+import stripe
+
+>>>>>>> 16eed07a0f (fix: use Stripe's Price API for plan-price information (#26107))
 import frappe
 import stripe
 from frappe import _
@@ -23,6 +28,7 @@ def create_stripe_subscription(gateway_controller, data):
 
 	except Exception:
 		frappe.log_error(frappe.get_traceback())
+<<<<<<< HEAD
 		return {
 			"redirect_to": frappe.redirect_to_message(
 				_("Server Error"),
@@ -31,6 +37,14 @@ def create_stripe_subscription(gateway_controller, data):
 				),
 			),
 			"status": 401,
+=======
+		return{
+			"redirect_to": frappe.redirect_to_message(
+				_('Server Error'),
+				_("It seems that there is an issue with the server's stripe configuration. In case of failure, the amount will get refunded to your account.")
+			),
+			"status": 401
+>>>>>>> 16eed07a0f (fix: use Stripe's Price API for plan-price information (#26107))
 		}
 
 
@@ -44,12 +58,17 @@ def create_subscription_on_stripe(stripe_settings):
 		customer = stripe.Customer.create(
 			source=stripe_settings.data.stripe_token_id,
 			description=stripe_settings.data.payer_name,
+<<<<<<< HEAD
 			email=stripe_settings.data.payer_email,
+=======
+			email=stripe_settings.data.payer_email
+>>>>>>> 16eed07a0f (fix: use Stripe's Price API for plan-price information (#26107))
 		)
 
 		subscription = stripe.Subscription.create(customer=customer, items=items)
 
 		if subscription.status == "active":
+<<<<<<< HEAD
 			stripe_settings.integration_request.db_set("status", "Completed", update_modified=False)
 			stripe_settings.flags.status_changed_to = "Completed"
 
@@ -58,6 +77,16 @@ def create_subscription_on_stripe(stripe_settings):
 			frappe.log_error("Subscription N°: " + subscription.id, "Stripe Payment not completed")
 	except Exception:
 		stripe_settings.integration_request.db_set("status", "Failed", update_modified=False)
+=======
+			stripe_settings.integration_request.db_set('status', 'Completed', update_modified=False)
+			stripe_settings.flags.status_changed_to = "Completed"
+
+		else:
+			stripe_settings.integration_request.db_set('status', 'Failed', update_modified=False)
+			frappe.log_error('Subscription N°: ' + subscription.id, 'Stripe Payment not completed')
+	except Exception:
+		stripe_settings.integration_request.db_set('status', 'Failed', update_modified=False)
+>>>>>>> 16eed07a0f (fix: use Stripe's Price API for plan-price information (#26107))
 		frappe.log_error(frappe.get_traceback())
 
 	return stripe_settings.finalize_request()
